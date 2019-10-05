8fbcb564dfb95086e6c1cd87bfa9b6f04cdeea93
Sergey Filin <sergey.filin@smartmail.biz>
2019-10-05 13:20:30 +0300

    init player

--- main.lua
+++ main.lua
@@ -1,22 +1,39 @@
-local assets, map, status
+local Bump, Player
+
+local assets, map, status, world, hero
 
 local statuses
-local player = {
-  status = 'default'
-}
+
+local ground_0 = {}
+local ground_1 = {}
 
 function love.load()
+    Bump = require 'bump.bump'
+    Player = require "player"
+
     assets = require "assets"
     map = require "map"
     status = require "status"
 
     statuses = {
-      default = status.new{ 0 },
-      fire_proof = status.new{ 0, 10 },
+        default = status.new { 0 },
+        fire_proof = status.new { 0, 10 },
     }
+
+    hero = Player:new()
+    hero:init()
+
+    world = Bump.newWorld(16) -- 16 is our tile size
+
+    world:add(hero, hero.x, hero.y, hero.img:getWidth(), hero.img:getHeight())
+
+    -- Draw a level
+    world:add(ground_0, 120, 360, 640, 16)
+    world:add(ground_1, 0, 448, 640, 32)
 end
 
 function love.update(dt)
+    hero:move(dt, world)
 end
 
 -- Draw a coloured rectangle.
@@ -24,10 +41,15 @@ function love.draw()
     love.graphics.setBackgroundColor(0, 0.4, 0.4)
     local height = #map
     for y, row in ipairs(map) do
-      for x, c in ipairs(row) do
-        if c == 1 then
-          love.graphics.draw(assets.wall, x * assets.w, y * assets.h)
+        for x, c in ipairs(row) do
+            if c == 1 then
+                love.graphics.draw(assets.wall, x * assets.w, y * assets.h)
+            end
         end
-      end
     end
+
+    love.graphics.rectangle('fill', world:getRect(ground_0))
+    love.graphics.rectangle('fill', world:getRect(ground_1))
+
+    hero:draw()
 end
---
+++ player.lua
@@ -0,0 +1,70 @@
+local Player = {}
+function Player:new()
+    local newObj = {
+        x = 16,
+        y = 16,
+        -- The first set of values are for our rudimentary physics system
+        xVelocity = 0, -- current velocity on x, y axes
+        yVelocity = 0,
+        acc = 100, -- the acceleration of our player
+        maxSpeed = 600, -- the top speed
+        friction = 20, -- slow our player down - we could toggle this situationally to create icy or slick platforms
+        gravity = 80, -- we will accelerate towards the bottom
+
+        -- These are values applying specifically to jumping
+        isJumping = false, -- are we in the process of jumping?
+        isGrounded = false, -- are we on the ground?
+        hasReachedMax = false, -- is this as high as we can go?
+        jumpAcc = 500, -- how fast do we accelerate towards the top
+        jumpMaxSpeed = 9.5, -- our speed limit while jumping
+
+        -- Here are some incidental storage areas
+        img = nil -- store the sprite we'll be drawing
+    }
+
+    self.__index = self
+
+    return setmetatable(newObj, self)
+end
+
+function Player:init()
+    self.img = love.graphics.newImage('assets/player.png')
+end
+
+function Player:move(dt, world)
+    local goalX = self.x + self.xVelocity
+    local goalY = self.y + self.yVelocity
+    local collisions
+
+    -- Apply Friction
+    self.xVelocity = self.xVelocity * (1 - math.min(dt * self.friction, 1))
+    self.yVelocity = self.yVelocity * (1 - math.min(dt * self.friction, 1))
+
+    -- Apply gravity
+    self.yVelocity = self.yVelocity + self.gravity * dt
+
+    if love.keyboard.isDown("left", "a") and self.xVelocity > -self.maxSpeed then
+        self.xVelocity = self.xVelocity - self.acc * dt
+    elseif love.keyboard.isDown("right", "d") and self.xVelocity < self.maxSpeed then
+        self.xVelocity = self.xVelocity + self.acc * dt
+    end
+
+    -- The Jump code gets a lttle bit crazy.  Bare with me.
+    if love.keyboard.isDown("up", "w") then
+        if -self.yVelocity < self.jumpMaxSpeed and not self.hasReachedMax then
+            self.yVelocity = self.yVelocity - self.jumpAcc * dt
+        elseif math.abs(self.yVelocity) > self.jumpMaxSpeed then
+            --            self.hasReachedMax = true
+        end
+
+        self.isGrounded = false -- we are no longer in contact with the ground
+    end
+
+    self.x, self.y, collisions = world:move(self, goalX, goalY)
+end
+
+function Player:draw()
+    love.graphics.draw(self.img, self.x, self.y)
+end
+
+return Player
